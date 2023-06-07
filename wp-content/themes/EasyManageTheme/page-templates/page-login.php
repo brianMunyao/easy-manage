f2c07174e4585b053ec2c05ac6b4b79dc247d58e
brianMunyao <brianmunyao6@gmail.com>
2023-06-07 15:03:30 +0300

    added: login page

--- wp-content/themes/EasyManageTheme/page-templates/page-login.php
+++ wp-content/themes/EasyManageTheme/page-templates/page-login.php
@@ -5,6 +5,44 @@
  * Template Name: Login Page Template
  */
 get_header() ?>
-login
+
+<?php
+
+if (isset($_POST['login'])) {
+    /**
+     * 
+     * TODO: handle submit here
+     */
+}
+
+?>
+
+<div class="login-page">
+    <form action="" method="post">
+        <div class="form">
+            <h2>Login</h2>
+
+            <div class="input-con">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Enter your email address">
+                </div>
+                <p class="form-error color-danger"><?php //error here
+                                                    ?></p>
+            </div>
+
+            <div class="input-con">
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter your password">
+                </div>
+                <p class="form-error color-danger"><?php //error here
+                                                    ?></p>
+            </div>
+
+            <button type="submit" class="app-btn primary-btn" name="login">Login</button>
+        </div>
+    </form>
+</div>
 
 <?php get_footer() ?>
